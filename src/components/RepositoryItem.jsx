9b65dfa46d66541479c5dc13a035ffe1658caa7a
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-28 19:53:50 -0400

    style: flexBox

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,6 +2,40 @@ import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import StyledText from './StyledText'
 
+const RepositoryStats = (props) => {
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Starts</StyledText>
+        <StyledText align='center'>{props.stargazersCount}</StyledText>
+      </View>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Forks</StyledText>
+        <StyledText align='center'>{props.forksCount}</StyledText>
+      </View>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Reviews</StyledText>
+        <StyledText align='center'>{props.reviewCount}</StyledText>
+      </View>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Rating</StyledText>
+        <StyledText align='center'>{props.ratingAverage}</StyledText>
+      </View>
+    </View>
+  )
+}
+
+const RepositoryItem = (props) => {
+  return (
+    <View key={props.id} style={styles.container}>
+      <StyledText fontSize='subheading' fontWeight='bold'>Fullname: {props.fullName}</StyledText>
+      <StyledText>{props.description}</StyledText>
+      <StyledText>{props.language}</StyledText>
+      <RepositoryStats {...props} />
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     padding: 20,
@@ -14,19 +48,5 @@ const styles = StyleSheet.create({
     marginBottom: 5
   }
 })
-
-const RepositoryItem = (props) => {
-  return (
-    <View key={props.id} style={styles.container}>
-      <StyledText fontSize='subheading' fontWeight='bold'>Fullname: {props.fullName}</StyledText>
-      <StyledText>{props.description}</StyledText>
-      <StyledText>{props.language}</StyledText>
-      <StyledText>Starts: {props.stargazersCount}</StyledText>
-      <StyledText>Forks: {props.forksCount}</StyledText>
-      <StyledText>Reviews: {props.reviewCount}</StyledText>
-      <StyledText>Rating: {props.ratingAverage}</StyledText>
-    </View>
-  )
-}
 
 export default RepositoryItem
