07df3046bd65aa3d696d6396c84e6e39fbe3005a
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-01-28 20:01:18 -0400

    feat: add util Parse Thousands

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,29 +1,7 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 import StyledText from './StyledText'
-
-const RepositoryStats = (props) => {
-  return (
-    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-      <View>
-        <StyledText align='center' fontWeight='bold'>Starts</StyledText>
-        <StyledText align='center'>{props.stargazersCount}</StyledText>
-      </View>
-      <View>
-        <StyledText align='center' fontWeight='bold'>Forks</StyledText>
-        <StyledText align='center'>{props.forksCount}</StyledText>
-      </View>
-      <View>
-        <StyledText align='center' fontWeight='bold'>Reviews</StyledText>
-        <StyledText align='center'>{props.reviewCount}</StyledText>
-      </View>
-      <View>
-        <StyledText align='center' fontWeight='bold'>Rating</StyledText>
-        <StyledText align='center'>{props.ratingAverage}</StyledText>
-      </View>
-    </View>
-  )
-}
+import RepositoryStats from './RepositoryStats'
 
 const RepositoryItem = (props) => {
   return (
---
+++ src/components/RepositoryStats.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { parseThousands } from '../utils/parseThousands'
+import { View } from 'react-native-web'
+import StyledText from './StyledText'
+
+const RepositoryStats = (props) => {
+  return (
+    <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Starts</StyledText>
+        <StyledText align='center'>{parseThousands(props.stargazersCount)}</StyledText>
+      </View>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Forks</StyledText>
+        <StyledText align='center'>{parseThousands(props.forksCount)}</StyledText>
+      </View>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Reviews</StyledText>
+        <StyledText align='center'>{parseThousands(props.reviewCount)}</StyledText>
+      </View>
+      <View>
+        <StyledText align='center' fontWeight='bold'>Rating</StyledText>
+        <StyledText align='center'>{parseThousands(props.ratingAverage)}</StyledText>
+      </View>
+    </View>
+  )
+}
+
+export default RepositoryStats
